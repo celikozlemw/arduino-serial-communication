7eefab677ee7586858af64da0959127df039ab31
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-07-03 10:03:38 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,15 +1,15 @@
 void setup() {
-  pinMode(A0, OUTPUT); 
+  pinMode(A1, OUTPUT); 
   Serial.begin(9600); 
 }
 
 void loop() {
-  digitalWrite(A0, HIGH); //high:1
+  int voltaj = analogRead(A1);
+  
+  digitalWrite(A1, HIGH); //high:1
   delay(2000); //2000ms bekle
-  digitalWrite(A0, LOW); //low:0
+  digitalWrite(A1, LOW); //low:0
   delay(2000); //2000ms bekle
 
-
-  int voltaj = analogRead(A0);
   Serial.println(voltaj);
 }
