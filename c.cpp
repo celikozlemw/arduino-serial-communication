37697bc9521e982457239cb6789b0ea963d1caaf
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-07-09 09:44:57 +0300

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,13 @@
+void setup() {
+
+  Serial.begin(9600);
+}
+void loop() {
+int analogValue = analogRead(A3);
+
+  float güç = analogValue * (5.0 / 1023.0) * (5.0 / 1023.0);
+
+  Serial.println(güç);
+
+delay(100);
+}
